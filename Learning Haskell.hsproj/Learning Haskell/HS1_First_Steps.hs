147eb60bd480fbf4794c77becc49385b0f24167a
Shawn Zhong <public@shawnzhong.com>
2018-06-01 16:27:30 +0800

    1 First Steps

--- Learning Haskell.hsproj/Learning Haskell/HS1_First_Steps.hs
+++ Learning Haskell.hsproj/Learning Haskell/HS1_First_Steps.hs
@@ -1,3 +1,16 @@
 module HS1_Fi1rst_Steps where
-  
-add x y = x + y 
+
+inc :: Num a => a -> a      -- type signature
+inc x = x + 1               -- function equation
+
+exclaim :: String -> String
+exclaim sentence = sentence ++ "!"
+
+average :: Float -> Float -> Float      -- Legal, right association of function definition
+--average :: Float -> (Float -> Float)  -- Legal, curried function
+--average :: (Float -> Float) -> Float  -- Illegal
+averagePair :: (Float, Float) -> Float  -- Legal, but different sigunature
+
+average      a  b  = (a + b) / 2
+averagePair (a, b) = (a + b) / 2
+
